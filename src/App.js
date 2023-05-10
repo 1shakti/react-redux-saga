c7c0b69e790183448df6d9e0b61e428eac18ba9d
1shakti <shakti26.sj@gmail.com>
2023-05-10 14:58:25 +0530

    basic action call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { addToCart } from "./redux/action";
+import { useDispatch } from "react-redux";
 
 function App() {
+
+  const dispatch = useDispatch();
+  const product = {
+    name: "i phone",
+    type: "mobile",
+    price: 10000,
+    color: "red"
+  }
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+    <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
   );
 }
 
